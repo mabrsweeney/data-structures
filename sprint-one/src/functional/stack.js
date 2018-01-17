d5cdfabff53f7a310760071a53095eedb1b8b710
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 11:08:37 -0800

    completed functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,14 +6,17 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    
     storage[size] = value;
     size++;
   };
 
   someInstance.pop = function() {
     if(size > 0){
-      delete storage[size];
+      var popVal = storage[size-1];
+      delete storage[size-1];
       size--;
+      return popVal;
     }
     
   };
