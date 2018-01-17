1bc5c9054f8456aed1b1f126b61817b75fc20002
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 13:14:46 -0800

    Implemented pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -7,6 +7,23 @@ var Queue = function() {
 
 Queue.prototype.size = function() {
   return this.count;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
 }
+
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    var output = this.storage[1];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[0];
+    this.count--;
+    return output;
+  }
+};
 
 
