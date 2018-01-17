4a504d576e21857e294df4517a5e2200bb1f1f4d
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 15:05:14 -0800

    Completed LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if ( list.head === null ) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var temp = list.head;
+      var removedHead = list.head.value;
+      list.head = list.head.next;
+      delete temp;
+      return removedHead;
+    }
   };
 
   list.contains = function(target) {
+    var itrNode = list.head;
+    while (itrNode !== null) {
+      if (itrNode.value === target) {
+        return true;
+      }
+      itrNode = itrNode.next;
+    }
+    return false;
   };
 
   return list;
