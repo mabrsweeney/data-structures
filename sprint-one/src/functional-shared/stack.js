b3d039592be344ec5d7778d3cf44e7547a9ae987
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 11:51:17 -0800

    Implemented stack creation function and stack.push

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  var newStack.storage = {};
+  var newStack.size = 0;
+  
+  _.extend(newStack, stackMethods);
+  
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.size++;
+    this.storage[size] = value;
+  }
+  
+};
 
 
