4484390186e969c9904c852b423b0d665d2b42e9
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 11:57:58 -0800

    Implemented stack object creation function (still needs methods)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  
+  newStack.count = 0;
+  newStack.storage = {};
+  
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  
+};
 
 
