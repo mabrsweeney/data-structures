11e7544ff137b2b018be2ea4e6d06fe6fd163a10
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 12:06:49 -0800

    Implemented pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -9,6 +9,18 @@ Stack.prototype.size = function() {
   return this.count;
 };
 
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    var output = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return output;
+  }
+};
 
 
