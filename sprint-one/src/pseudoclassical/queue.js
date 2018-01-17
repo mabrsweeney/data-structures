15e44a4f32603e3c89f0e402c1eb78792cb81de4
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 13:11:42 -0800

    Implemented pseudoclassical queue constructor and size method

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,12 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
+
+Queue.prototype.size = function() {
+  return this.count;
+}
 
 
