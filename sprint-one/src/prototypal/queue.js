fc330d4f29cc3471955cc87f80a368329727131e
mabrsweeney <mabrsweeney@gmail.com>
2018-01-17 13:05:02 -0800

    Implemented prototypal object construction function and size method

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.count = 0;
+  newQueue.storage = {};
+  
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.count;
+  }
+};
 
 
