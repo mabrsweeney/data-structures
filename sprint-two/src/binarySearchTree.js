1ef2e6471f779750538b5b1c36fbc9101b83e3e0
mabrsweeney <mabrsweeney@gmail.com>
2018-01-18 10:36:26 -0800

    constructed BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,24 @@
 var BinarySearchTree = function(value) {
+  var newBinarySearchTree = Object.create(BinarySearchTree.methods);
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+  newBinarySearchTree.value = value;
+  return newBinarySearchTree;
 };
 
+BinarySearchTree.methods = {};
 
+BinarySearchTree.methods.insert = function(value) {
+  
+};
+
+BinarySearchTree.methods.contains = function(value) {
+
+};
+
+BinarySearchTree.methods.DepthFirstLog = function(cb) {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
