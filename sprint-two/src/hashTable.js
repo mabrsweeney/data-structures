0941629cb025f1d29a73f696548289824ae2336b
mabrsweeney <mabrsweeney@gmail.com>
2018-01-18 10:11:36 -0800

    completed hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,23 +3,41 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._keyStorage = {};
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //while (this._storage.get(index) !== undefined) {
-  //  index++;  
-  //}
+  while (this._keyStorage.hasOwnProperty(index) && this._keyStorage[index] !== k) {
+    index = (index + 1) % this._limit;
+
+  }
+
+  this._keyStorage[index] = k;
   this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var startIndex = index;
+  while (this._keyStorage[index] !== k) { 
+    index = (index + 1) % this._limit;
+    if (index === startIndex) {
+      return undefined;
+    }
+  }
+
   return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  while (this._keyStorage[index] !== k) { 
+    index = (index + 1) % this._limit;
+    if (index === startIndex) {
+      return undefined;
+    }
+  }
   this._storage.set(index, undefined);
   
 };
