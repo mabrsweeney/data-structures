ccd79e3a2ed61799f1dc40a6395cfca40bf4dd7c
mabrsweeney <mabrsweeney@gmail.com>
2018-01-19 11:41:26 -0800

    refactored hash table to use linked lists

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,20 +3,64 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._keyStorage = {};
   this._size = 0;
 };
+
+HashTable.prototype.getTupleNode = function(bucket, k, isPrev) {
+  var curNode = bucket.head;
+  if (curNode.value[0] === k) {
+    return curNode;
+  } else if (curNode.next.value[0] === k) {
+    if (isPrev) {
+      return curNode;
+    } else {
+      return curNode.next;
+    }
+  } else {
+    while (curNode.next !== bucket.tail) {
+      curNode = curNode.next;
+      if (curNode.next.value[0] === k) {
+        if (isPrev) {
+          return curNode;
+        } else {
+          return curNode.next;
+        }
+      }
+    }
+  }
+};
+
 
 HashTable.prototype.insert = function(k, v) {
 
   
-  if (this._size >= this._limit * .75) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket === undefined) {
+    var newLList = LinkedList();
+    newLList.addToTail([k, v]);
+    this._storage.set(index, newLList);
+  } else {
+    var node = this.getTupleNode(bucket, k, false);
+    if (node === undefined) {
+      bucket.addToTail([k, v]);
+    } else {
+      node.value[1] = v;
+    }
+    
+  }
+  this._size++;
+  
+  //check for reSize;
+  
+/*  if (this._size >= this._limit * .75) {
     this.reHash(this._limit + this._limit);
     
   } else if (this._size <= this._limit * .25) {
     this.reHash(Math.ceil(this._limit / 2));
   } 
-  
+  // index is out of bounds because the old limit is still being used
+  // after the Hashtable is resized.
   var index = getIndexBelowMaxForKey(k, this._limit);
   while (this._keyStorage.hasOwnProperty(index) && this._keyStorage[index] !== k) {
     index = (index + 1) % this._limit;
@@ -24,12 +68,30 @@ HashTable.prototype.insert = function(k, v) {
 
   this._keyStorage[index] = k;
   this._storage.set(index, v);
-  this._size++;
+  this._size++;*/
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var startIndex = index;
+  var bucket = this._storage.get(index);
+  
+  if (bucket === undefined || (bucket.head === null)) {
+    return undefined;
+  }
+  
+  var curNode = bucket.head;
+  if (curNode.value[0] === k) {
+    return curNode.value[1];
+  }
+  
+  while (curNode !== bucket.tail) {
+    curNode = curNode.next;
+    if (curNode.value[0] === k) {
+      return curNode.value[1];
+    }
+  }
+  
+  /*var startIndex = index;
   while (this._keyStorage[index] !== k) { 
     index = (index + 1) % this._limit;
     if (index === startIndex) {
@@ -37,16 +99,56 @@ HashTable.prototype.retrieve = function(k) {
     }
   }
 
-  return this._storage.get(index);
+  return this._storage.get(index);*/
 };
 
 HashTable.prototype.remove = function(k) {
+  
+  var removeNode = function(prevNode) {
+    if (prevNode.next === bucket.tail) {
+      prevNode.next = null;
+      bucket.tail = prevNode;
+    } else {
+      prevNode.next = prevNode.next.next;
+    }
+  };
+  
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  
+  if (bucket === undefined) {
+    return undefined;
+  }
+  
+  var curNode = this.getTupleNode(bucket, k, true);
+  if (curNode === undefined) {
+    return undefined;
+  } else if (curNode === bucket.head) {
+    bucket.removeHead();
+  } else {
+    removeNode(curNode);
+  }
+  
+  this._size--;
+  /*var curNode = bucket.head;
+  if (curNode.value[0] === k) {
+    bucket.removeHead();
+  } else if (curNode.next.value[0] === k) {
+    removeNode(curNode);
+  } else {
+    while (curNode.next !== bucket.tail) {
+      curNode = curNode.next;
+      if (curNode.next.value[0] === k) {
+        removeNode(curNode);
+      }
+    }
+  }*/
+  /*var index = getIndexBelowMaxForKey(k, this._limit);
   while (this._keyStorage[index] !== k) { 
     index = (index + 1) % this._limit;
   }
   this._storage.set(index, undefined);
-  this._size--;
+  this._size--;*/
 };
 
 HashTable.prototype.reHash = function(newLimit) {
@@ -55,6 +157,8 @@ HashTable.prototype.reHash = function(newLimit) {
   this._keyStorage = {};
   this._storage = LimitedArray(newLimit);
   this._size = 0;
+  //Limit needs to be set before rehashing begins.
+
   for (var i = 0; i < this._limit; i++) {
     if (tempStorage.get(i) !== undefined) {
       var newKey = tempKeyStorage[i];
