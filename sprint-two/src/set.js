5232885891f05ce59aa8b13e897d5a91cdac0fdf
mabrsweeney <mabrsweeney@gmail.com>
2018-01-20 14:50:26 -0800

    Added comments to set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,14 +6,23 @@ var Set = function() {
 
 var setPrototype = {};
 
+// Input:   A new item
+// Output:  N/A
+// Purpose: Adds a new item to the set
 setPrototype.add = function(item) {
   this._storage[item] = item;
 };
 
+// Input:   An item
+// Output:  A boolean value
+// Purpose: Checks if the item is within the set
 setPrototype.contains = function(item) {
   return this._storage.hasOwnProperty(item);
 };
 
+// Input:   An item
+// Output:  N/A
+// Purpose: Removes an item from the set
 setPrototype.remove = function(item) {
   delete this._storage[item];
 };
